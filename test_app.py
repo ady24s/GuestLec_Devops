4c3c33aa4367738974c61c0480f002928ee8ee5f
ady24s <adyasha24@gmail.com>
2025-09-13 10:13:49 +0530

    Ran test cases

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5
-   #assert add(-1, 1) == 0
-    #assert add(0, 0) == 0
-    #assert add(-1, -1) == -2
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-1, -1) == -2
